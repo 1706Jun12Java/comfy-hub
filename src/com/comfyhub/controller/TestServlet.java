b4ce4e350e075b491fe7dcfe023235f8e0b62687
mridulazaman <mridula.m.zaman@gmail.com>
2017-07-27 10:41:43 -0400

    webXML

--- src/com/comfyhub/controller/TestServlet.java
+++ src/com/comfyhub/controller/TestServlet.java
@@ -2,31 +2,20 @@ package com.comfyhub.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TestServlet
- */
-@WebServlet("/TestServlet")
+
+//@WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+   
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		System.out.println("We here!!");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
